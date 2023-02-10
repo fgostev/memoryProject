74cc7200c62d715a516e23d329fef30292e8f21a
fgostev <fgostev@gmail.com>
2023-02-09 19:03:54 -0600

    work in progress - saving clicked cards and score

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import GameBoard from "./components/gameBoard.component";
+import {useState, useEffect} from 'react';
+
+// import GameBoard from "./components/gameBoard.component";
+import Header from "./components/header.component";
+import Images from './components/images.component';
+import Card from './components/card.component';
+
 
 function App() {
+
+  const [posters, setPosters] = useState(Images);
+  const [score, setScore] = useState([0, 0]);
+  const [clickedCards, setClickedCards] = useState([])
+
   return (
     <div className="App">
-          <GameBoard />
+          <Header
+          score = {score}
+          setScore = {setScore} />
+          <Card posters = {posters}
+            setPosters = {setPosters}
+            clickedCards = {clickedCards}
+            setClickedCards = {setClickedCards}
+            score = {score}
+            setScore = {setScore}
+          />
+          {/* <GameBoard />  */}
     </div>
   );
 }
---
+++ src/components/header.component.jsx
@@ -0,0 +1,15 @@
+const Header =({score, setScore}) =>{
+    return(
+        <div id="header"> 
+        <div id="headerLeft">
+            <h1>Movie Memory Game</h1>
+        </div>
+        <div id="headerRight"> 
+           <span> Score: {score[0]} </span> <br></br>
+           <span> Best Score: {score[1]} </span>
+        </div>
+        </div>
+    )
+}
+
+export default Header;
