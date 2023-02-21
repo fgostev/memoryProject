cb6668374b07cd6e72e3eab02b05043e18347e3a
fgostev <fgostev@gmail.com>
2023-02-21 15:43:14 -0600

    cleanUp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
 // import GameBoard from "./components/gameBoard.component";
 import Header from "./components/header.component";
